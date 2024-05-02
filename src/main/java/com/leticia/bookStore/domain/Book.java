bb71b01b6163afb4d888d241166045cb30e0cb20
leticiafdepaula <leticiaferreira150998@gmail.com>
2024-05-02 11:05:51 -0300

    finalizando as classes de dominio

--- src/main/java/com/leticia/bookStore/domain/Book.java
+++ src/main/java/com/leticia/bookStore/domain/Book.java
@@ -1,18 +1,27 @@
 package com.leticia.bookStore.domain;
 
+import jakarta.persistence.*;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.io.Serializable;
+
 @NoArgsConstructor
 @Getter
 @Setter
 @EqualsAndHashCode
-public class Book {
+@Entity
+public class Book implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String titulo;
     private String nome_autor;
     private String texto;
+    @ManyToOne
+    @JoinColumn (name = "category_id")
     private Category category;
 }
--- src/main/java/com/leticia/bookStore/domain/Category.java
+++ src/main/java/com/leticia/bookStore/domain/Category.java
@@ -1,9 +1,12 @@
 package com.leticia.bookStore.domain;
 
+import jakarta.persistence.*;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
+
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -11,11 +14,17 @@ import java.util.List;
 @Setter
 @RequiredArgsConstructor
 @EqualsAndHashCode
-public class Category {
+@Entity
+
+public class Category implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String nome;
     private String descricao;
 
+    @OneToMany(mappedBy = "category")
     private List<Book> books = new ArrayList<>();
 
 }
